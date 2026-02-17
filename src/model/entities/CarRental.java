fde4ff6f713b7c51606126df294f9cbadee0dc35
henrryribeiro <henrry.marcell@gmail.com>
2026-02-17 17:44:45 -0300

    solução do problema sem usar interface

--- src/model/entities/CarRental.java
+++ src/model/entities/CarRental.java
@@ -7,12 +7,41 @@ public class CarRental {
     private LocalDateTime start;
     private LocalDateTime finish;
 
+    private Vehicle vehicle;
+    private Invoice invoice;
+
     public CarRental() {
 
     }
 
-    public CarRental(LocalDateTime start, LocalDateTime finish) {
+    public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
         this.start = start;
+        this.finish = finish;
+        this.vehicle = vehicle;
+        this.invoice = invoice;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public LocalDateTime getFinish() {
+        return finish;
+    }
+
+    public void setFinish(LocalDateTime finish) {
         this.finish = finish;
     }
 
@@ -22,13 +51,5 @@ public class CarRental {
 
     public void setStart(LocalDateTime start) {
         this.start = start;
-    }
-
-    public LocalDateTime getFinish() {
-        return finish;
-    }
-
-    public void setFinish(LocalDateTime finish) {
-        this.finish = finish;
     }
 }
